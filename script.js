054da4ad6ac32a935becdd66c937ef74cb513515
Yoon Lee <himynameisyoon@gmail.com>
2017-09-03 17:00:58 -0700

    finish styling for desktop

---
+++ script.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+  $(".header-bot").hover(
+    function() {
+      $(this).fadeOut(100, function() {
+        $(this).css({
+          "background": "#2C3E50"
+        }).fadeIn(100);
+        $(this).children().children().css({
+          "color": "#fff"
+        }).fadeIn(100);
+      });
+    },
+    function() {
+      $(this).fadeOut(100, function() {
+        $(this).css({
+          "background": "none"
+        }).fadeIn(100);
+        $(this).children().children().css({
+          "color": "#444"
+        }).fadeIn(100);
+      });
+    }
+  );
+});
